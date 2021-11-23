d44411ef1512efd698e3ff69cf14fadad7ade122
grayson-poon <grayson.a.poon@gmail.com>
2021-11-22 17:22:47 -0800

    change for heroku push

--- frontend/components/users/first_last_name_and_pronouns.jsx
+++ frontend/components/users/first_last_name_and_pronouns.jsx
@@ -1,38 +1,38 @@
-import React from "react";
+// import React from "react";
 
-export const firstLastNameAndPronouns = () => {
-  return (
-    <form onSubmit={this.nextForm}>
-      <label>
-        First Name*
-        <input
-          type="text"
-          onChange={this.update("firstName")}
-          value={this.state.firstName}
-          required
-        />
-      </label>
+// export const firstLastNameAndPronouns = () => {
+//   return (
+//     <form onSubmit={this.nextForm}>
+//       <label>
+//         First Name*
+//         <input
+//           type="text"
+//           onChange={this.update("firstName")}
+//           value={this.state.firstName}
+//           required
+//         />
+//       </label>
 
-      <label>
-        Last Name*
-        <input
-          type="text"
-          onChange={this.update("lastName")}
-          value={this.state.lastName}
-          required
-        />
-      </label>
+//       <label>
+//         Last Name*
+//         <input
+//           type="text"
+//           onChange={this.update("lastName")}
+//           value={this.state.lastName}
+//           required
+//         />
+//       </label>
 
-      <label>
-        Preferred Pronouns
-        <input
-          type="text"
-          onChange={this.update("pronouns")}
-          value={this.state.pronouns}
-        />
-      </label>
+//       <label>
+//         Preferred Pronouns
+//         <input
+//           type="text"
+//           onChange={this.update("pronouns")}
+//           value={this.state.pronouns}
+//         />
+//       </label>
 
-      <input type="submit" value="Next" />
-    </form>
-  );
-};
+//       <input type="submit" value="Next" />
+//     </form>
+//   );
+// };
