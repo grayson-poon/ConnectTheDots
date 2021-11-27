0b32efce715fa3ab3189ec6d75226a99c9512751
grayson-poon <grayson.a.poon@gmail.com>
2021-11-26 20:36:41 -0800

    render select footer upon url pathname

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,45 +4,73 @@ import * as UrlPath from "../../util/url_paths_util";
 
 export default class Footer extends React.Component {
   render() {
-    switch(this.props.url) {
-      case UrlPath.SPLASH:
-        return this.splashFooter();
-      case UrlPath.USER_SHOW:
-        return this.splashFooter();
-      default:
-        return this.defaultFooter();
-    }
+    // switch(this.props.url) {
+    //   case UrlPath.SPLASH:
+    //     return this.splashFooter();
+    //   case UrlPath.USER_SHOW:
+    //     return this.splashFooter();
+    //   default:
+    //     return this.defaultFooter();
+    // }
+
+    return this.props.url === UrlPath.SIGNUP_FORM ||
+      this.props.url === UrlPath.LOGIN_FORM
+        ? this.formFooter()
+        : this.splashFooter();
+
   }
 
-  defaultFooter() {
+  // defaultFooter() {
+  //   return (
+  //     <div className="default-footer">
+  //       <div className="list-1">
+  //         <ul>
+  //           <li>
+  //             <img src={LOGO_URL} />
+  //           </li>
+  //           <li>Javascript</li>
+  //           <li>Javascript</li>
+  //           <li>Javascript</li>
+  //           <li>ConnectTheDots 2021</li>
+  //         </ul>
+  //       </div>
+  //       <div className="list-2">
+  //         <ul>
+  //           <li>HIDE ME</li>
+  //           <li>Javascript</li>
+  //           <li>Javascript</li>
+  //           <li>Javascript</li>
+  //         </ul>
+  //       </div>
+  //       <div className="list-3">
+  //         <ul>
+  //           <li>HIDE ME</li>
+  //           <li>Javascript</li>
+  //           <li>Javascript</li>
+  //           <li>Javascript</li>
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+  formFooter() {
     return (
-      <div className="default-footer">
-        <div className="list-1">
-          <ul>
-            <li>
-              <img src={LOGO_URL} />
-            </li>
-            <li>Javascript</li>
-            <li>Javascript</li>
-            <li>Javascript</li>
-            <li>ConnectTheDots 2021</li>
-          </ul>
-        </div>
-        <div className="list-2">
-          <ul>
-            <li>HIDE ME</li>
-            <li>Javascript</li>
-            <li>Javascript</li>
-            <li>Javascript</li>
-          </ul>
-        </div>
-        <div className="list-3">
-          <ul>
-            <li>HIDE ME</li>
-            <li>Javascript</li>
-            <li>Javascript</li>
-            <li>Javascript</li>
-          </ul>
+      <div className="formFooter">
+        <div className="bottom-level">
+          <div className="statement">
+            <p>A FEW THINGS YOU SHOULD KNOW</p>
+            <p>
+              ConnectTheDots is a clone of LinkedIn, the professional networking
+              application. ConnectTheDots is not affiliated with LinkedIn in any
+              form, and is also not a real platform for any professional
+              development/networking. If you create a profile with your own
+              personal information, nothing will be done with it, however it
+              will likely be saved with this application for a long, long time.
+            </p>
+          </div>
+
+          <div className="name-year">2021 Grayson Poon</div>
         </div>
       </div>
     );
