42969c20adc38c61b9fd70ff3f6d55a1687b6043
grayson-poon <grayson.a.poon@gmail.com>
2021-11-28 14:25:14 -0800

    update post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,17 @@
 class PostsController < ApplicationController
 
-  def create
-    
+  def index
+    # @posts = Post.select(*).where({ user_id: { current_user.id }})
   end
 
+  def create
+    @post = Post.new(post_params)
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id, :photo)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  
+  validates :body, :user_id, presence: true
+
+  has_one_attached :photo
 
 end
