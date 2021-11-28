fa40d9d27a98b22c4fbc9160229176dd2bd4752e
grayson-poon <grayson.a.poon@gmail.com>
2021-11-28 12:57:40 -0800

    bootstrapping

--- frontend/connect_the_dots.jsx
+++ frontend/connect_the_dots.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./components/root";
 import configureStore from "./store/store";
+import { logout } from "./actions/session_actions";
 
 document.addEventListener("DOMContentLoaded", () => {
   let preloadedState;
   if (window.currentUserId) {
-    preloadedState = {
-      session: {
-        currentUserId: window.currentUserId,
-      }
-    }
+    // preloadedState = {
+    //   session: {
+    //     currentUserId: window.currentUserId,
+    //   }
+    // }
 
     // delete window.currentUserId;
   }
