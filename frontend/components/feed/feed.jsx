0769b71e0b11531d1a4df15e82718e56c85a5499
grayson-poon <grayson.a.poon@gmail.com>
2021-11-29 14:11:27 -0800

    create separate component for left profile box

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { DEFAULT_BACKGROUND_PICTURE, DEFAULT_PROFILE_PICTURE } from "../../util/images_and_icons_util";
-import { FEED, MY_NETWORK, USER_SHOW } from "../../util/url_paths_util";
+import { FEED, MY_NETWORK } from "../../util/url_paths_util";
 import PostIndexContainer from "../posts/post_index_container";
-// import PostIndex from "../";
+import LeftProfileBox from "./left_profile_box";
 
 export default class Feed extends React.Component {
   constructor(props) {
@@ -11,8 +11,9 @@ export default class Feed extends React.Component {
   }
 
   render() {
-    if (!this.props.currentUser && this.props.currentUserId)
+    if (!this.props.currentUser && this.props.currentUserId) {
       this.props.fetchUser(this.props.currentUserId);
+    }
     
     switch(this.props.url) {
       case FEED:
@@ -29,38 +30,12 @@ export default class Feed extends React.Component {
 
     return (
       <div className="whole-feed-page">
-        <div className="left-profile-box">
-          <div className="left-profile-background">
-            <img src={DEFAULT_BACKGROUND_PICTURE} />
-          </div>
+        <LeftProfileBox
+          currentUser={currentUser}
+          currentUserId={currentUserId}
+        />
 
-          <div className="left-profile-image">
-            <img
-              src={
-                currentUser.profilePicture
-                  ? currentUser.profilePicture
-                  : DEFAULT_PROFILE_PICTURE
-              }
-            />
-          </div>
-
-          <div className="left-profile-info">
-            <Link to={`/users/${currentUserId}`}>
-              {currentUser.firstName} {currentUser.lastName}
-            </Link>
-            <li>{currentUser.headline}</li>
-          </div>
-
-          <div className="left-profile-connections">
-            <Link to={MY_NETWORK}>
-              <div id="text">Number of connections</div>
-
-              <div id="number">100</div>
-            </Link>
-          </div>
-        </div>
-
-        {/* <PostIndexContainer currentUserId={currentUserId} /> */}
+        <PostIndexContainer currentUserId={currentUserId} />
       </div>
     );
   }
---
+++ frontend/components/feed/left_profile_box.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { DEFAULT_BACKGROUND_PICTURE, DEFAULT_PROFILE_PICTURE } from "../../util/images_and_icons_util";
+import { MY_NETWORK } from "../../util/url_paths_util";
+import { Link } from "react-router-dom";
+
+const LeftProfileBox = ({ currentUser, currentUserId }) => {
+  return (
+    <div className="left-profile-box">
+      <div className="left-profile-background">
+        <img src={DEFAULT_BACKGROUND_PICTURE} />
+      </div>
+
+      <div className="left-profile-image">
+        <img
+          src={
+            currentUser.profilePicture
+              ? currentUser.profilePicture
+              : DEFAULT_PROFILE_PICTURE
+          }
+        />
+      </div>
+
+      <div className="left-profile-info">
+        <Link to={`/users/${currentUserId}`}>
+          {currentUser.firstName} {currentUser.lastName}
+        </Link>
+        <li>{currentUser.headline}</li>
+      </div>
+
+      <div className="left-profile-connections">
+        <Link to={MY_NETWORK}>
+          <div id="text">Number of connections</div>
+          <div id="number">100</div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LeftProfileBox;
