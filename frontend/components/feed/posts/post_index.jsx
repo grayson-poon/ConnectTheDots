510f812f74daedbc4e321e4cacf61d55152b81c5
grayson-poon <grayson.a.poon@gmail.com>
2021-12-28 11:52:24 -0800

    style profile pictures and refactor the activity and feed section

--- frontend/components/feed/posts/post_index.jsx
+++ frontend/components/feed/posts/post_index.jsx
@@ -17,31 +17,64 @@ export default class PostIndex extends React.Component {
   }
 
   render() {
-    let { url, posts, users, currentUser } = this.props;
+    let { url } = this.props;
     
+    switch(url) {
+      case FEED:
+        return this.showFeed();
+      default:
+        return this.showActivity();
+    }
+  }
+
+  showFeed() {
+    let { currentUser } = this.props;
+
+    return (
+      <div className="post-index">
+        <NewPostForm currentUser={currentUser} />
+        {this.postsList()}
+      </div>
+    );
+  }
+
+  showActivity() {
     return (
       <div className="post-index-background">
         <div className="post-index">
-          {url === FEED ? <NewPostForm currentUser={currentUser} /> : null}
-
-          {Object.values(posts).length === 0 ? null : (
-            <div className="posts-list">
-              <ul>
-                {Object.values(posts)
-                  .reverse()
-                  .map((post) => (
-                    <PostIndexItem
-                      key={post.id}
-                      post={post}
-                      user={users[post.userId]}
-                      currentUser={currentUser}
-                    />
-                  ))}
-              </ul>
-            </div>
-          )}
+          {this.postsList() || this.zeroActivity()}
         </div>
       </div>
-    );
+    )
+  }
+
+  postsList() {
+    let { currentUser, posts, users } = this.props;
+    if (Object.values(posts).length === 0) return null;
+
+    return (
+      <div className="posts-list">
+        <ul>
+          {Object.values(posts)
+            .reverse()
+            .map((post) => (
+              <PostIndexItem
+                key={post.id}
+                post={post}
+                user={users[post.userId]}
+                currentUser={currentUser}
+              />
+            ))}
+        </ul>
+      </div>
+    )
+  }
+
+  zeroActivity() {
+    return (
+      <div className="activty-header">
+        <div>No activity yet!</div>
+      </div>
+    )
   }
 }
